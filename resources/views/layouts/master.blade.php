e61bfb759a5a392ec5ff899811f0211d73997b79
Panggih <tridarmapanggih@gmail.com>
2022-09-03 19:06:58 +0800

    first push

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc." />
+
+    @include('includes.styles')
+
+    <!-- Title -->
+    <title>Dashkit</title>
+
+  </head>
+  <body>
+    <!-- NAVIGATION
+    ================================================== -->
+
+
+    @include('includes.sidebar')
+
+
+
+
+
+
+    <!-- MAIN CONTENT
+    ================================================== -->
+    <div class="main-content">
+
+
+
+      @yield('content')
+
+    </div> <!-- / .main-content -->
+
+    <!-- JAVASCRIPT
+    ================================================== -->
+    @include('includes.scripts')
+
+
+  </body>
+</html>
